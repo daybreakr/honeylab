7b1181e07c8ad3d41e6321c16d13944b68795464
ethan <ethan@sz-nestech.com>
2018-07-25 16:00:35 +0800

    Added 'keepaliveCompat' flavor'

---
+++ app-lab/src/keepaliveCompat/java/com/honeycomb/sdk/ModRegistry.java
@@ -0,0 +1,31 @@
+package com.honeycomb.sdk;
+
+import android.content.Context;
+
+import com.honeycomb.mod.heartbeat.Heartbeat;
+import com.honeycomb.mod.keepalive.keepalive.KeepAlive;
+import com.honeycomb.mod.keepalive.keepalive.KeepAliveOptions;
+import com.honeycomb.mod.keepalive.wakeup.Wakeup;
+import com.honeycomb.sdk.common.AppCommonRegistry;
+
+public class ModRegistry extends AppCommonRegistry {
+
+    @Override
+    public void onConfigure(Context context) {
+        KeepAliveOptions keepAliveOptions = new KeepAliveOptions();
+        keepAliveOptions.enableForegroundService = false;
+        KeepAlive.setOptions(keepAliveOptions);
+    }
+
+    @Override
+    public void onStart(Context context) {
+        // Start heartbeat
+        Heartbeat.getInstance().start();
+
+        // Keep wake-up
+        Wakeup.getInstance().start();
+
+        // Keep alive
+        KeepAlive.getInstance().start();
+    }
+}
