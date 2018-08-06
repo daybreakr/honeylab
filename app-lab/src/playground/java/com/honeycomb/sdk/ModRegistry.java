3fed336781632259a1baf5a4e2faa1c477c66c49
ethan <ethanye@foxmail.com>
2018-08-06 16:49:30 +0800

    Added 'ModRegistry' for 'playground'.

---
+++ app-lab/src/playground/java/com/honeycomb/sdk/ModRegistry.java
@@ -0,0 +1,18 @@
+package com.honeycomb.sdk;
+
+import android.content.Context;
+
+import com.honeycomb.sdk.common.AppCommonRegistry;
+
+public class ModRegistry extends AppCommonRegistry {
+
+    @Override
+    public void onConfigure(Context context) {
+
+    }
+
+    @Override
+    public void onStart(Context context) {
+
+    }
+}
